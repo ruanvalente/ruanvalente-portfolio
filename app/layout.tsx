fe607082c959fb28040a1393e63dbe6d568576b5
ruanvalente <ruanopensuselinux@gmail.com>
2025-04-06 22:10:42 -0300

    feat: integrate translation functionality into Footer component and update copyright text

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,9 @@ export default function RootLayout({
           <LanguageProvider>
             <Header />
             {children}
+            <Footer />
           </LanguageProvider>
         </ThemeProvider>
-        <Footer />
       </body>
     </html>
   );
