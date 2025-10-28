85c591c7451b1ca8834d84ab6287e617a7dade44
ruanvalente <ruanopensuselinux@gmail.com>
2025-10-28 18:27:30 -0300

    feat: simplify Footer component by removing social links and updating copyright year

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,42 +1,17 @@
 "use client";
 
 import { useLanguage } from "@/context/language-context";
-import { Github, Linkedin, Mail } from "lucide-react";
 
 export function Footer() {
   const { t } = useLanguage();
   return (
     <footer className="bg-card py-10 border-t">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
+        <div className="flex items-end">
+          <div className="ml-auto mb-4 text-center md:mb-0 md:text-right">
             <p className="text-muted-foreground">
-              © {new Date().getFullYear()} Ruan Valente. {t("footer.rights")}
+              © 2025 Ruan Valente. {t("footer.rights")}
             </p>
-          </div>
-          <div className="flex gap-4">
-            <a
-              href="https://github.com/ruanvalente"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              <Github size={20} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/ruan-valente"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              <Linkedin size={20} />
-            </a>
-            <a
-              href="mailto:info@ruanvalente.dev"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              <Mail size={20} />
-            </a>
           </div>
         </div>
       </div>
